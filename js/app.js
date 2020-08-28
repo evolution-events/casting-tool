5b5cddb43f91df47d931428314df5be516a394ad
René van den Berg <rene@ranj.nl>
2020-08-28 23:42:20 +0200

    Move configuration to an external file to make reuse easier

--- js/app.js
+++ js/app.js
@@ -1,33 +1,12 @@
 import sortable from './html5sortable.es.js';
 
-/// Configuration section. This should be fetched from another location. /////////////////////////////////////////////////////////////////////////////////////////
+/// The variable 'config' needs to be externally defined.
 
-var options = [
-    "Aphrodite",
-    "Apollo",
-    "Artemis",
-    "Ares",
-    "Athena",
-    "Dionysos",
-    "Demeter",
-    "Hades",
-    "Eris",
-    "Hephaistos",
-    "Hecate",
-    "Hermes",
-    "Hera",
-    "Poseidon",
-    "Persephone",
-    "Zeus",
-];
-
-var formUrl = "https://docs.google.com/forms/d/e/1FAIpQLScJexu0GSXanv_nUrpn6xFM4KOiHvZ4wV31IucbSNy5oh_niw/viewform?usp=pp_url&entry.1827146005=$NAME&entry.1793455202=$CHARACTERS";
-
-var minSelectedCharacters = 5;
-
-var list = document.getElementById('casting-options');
+var { options, formUrl, minSelectedCharacters } = config;
 
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+var list = document.getElementById('casting-options');
 
 options.sort().forEach((option) => {
     var item = document.createElement('li');
---
+++ js/casting-tool-config.js
@@ -0,0 +1,22 @@
+var config = {
+   options: [
+    "Aphrodite",
+    "Apollo",
+    "Artemis",
+    "Ares",
+    "Athena",
+    "Dionysos",
+    "Demeter",
+    "Hades",
+    "Eris",
+    "Hephaistos",
+    "Hecate",
+    "Hermes",
+    "Hera",
+    "Poseidon",
+    "Persephone",
+    "Zeus",
+  ],
+  formUrl: "https://docs.google.com/forms/d/e/1FAIpQLScJexu0GSXanv_nUrpn6xFM4KOiHvZ4wV31IucbSNy5oh_niw/viewform?usp=pp_url&entry.1827146005=$NAME&entry.1793455202=$CHARACTERS",
+  minSelectedCharacters: 5,
+};
